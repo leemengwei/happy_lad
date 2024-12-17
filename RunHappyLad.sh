210242e23185ae66b31fe73bd170686727b60296
Your Name <you@example.com>
2024-12-17 08:19:56 -0800

    up

--- RunHappyLad.sh
+++ RunHappyLad.sh
@@ -5,6 +5,6 @@ echo "=====Happy Lad Recorder====="
 sleep 1
 echo "Happy Lad Forever Run..."
 cd /home/feifeichouchou/happy_lad/src/
-/usr/bin/python3 web.py &
+/usr/bin/python3 /home/feifeichouchou/happy_lad/src/web.py &
 /usr/bin/python3 /home/feifeichouchou/happy_lad/src/deepstream_test_1_usb.py /dev/video0
 echo "All The Best"
